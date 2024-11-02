07d18cb67c0dfbfc621713b3cb73d2e239f9f77c
Alan Kunz Cechinel <alan.cechinel@sc.senai.br>
2024-11-02 11:06:37 -0300

    initial action implementation from ros tutorial

--- Q2/pkg_3/include/pkg_3/prime_numbers.hpp
+++ Q2/pkg_3/include/pkg_3/prime_numbers.hpp
@@ -2,15 +2,26 @@
 #define PKG_3__PRIME_NUMBERS_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "q2_interfaces/action/prime_number.hpp"
 
 namespace pkg_3
 {
 class PrimeNumbers : public rclcpp::Node
 {
 public:
+    using PrimeNumberInterface = q2_interfaces::action::PrimeNumber;
+    using GoalHandlePrimeNumber = rclcpp_action::ServerGoalHandle<PrimeNumberInterface>;
+
     PrimeNumbers();
 
 private:
+    rclcpp_action::Server<PrimeNumberInterface>::SharedPtr action_server_;
+
+    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const PrimeNumberInterface::Goal> goal);
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle);
+    void handle_accepted(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle);
+    void execute(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle);
 
 };
 }  // namespace pkg3
--- Q2/pkg_3/src/prime_numbers.cpp
+++ Q2/pkg_3/src/prime_numbers.cpp
@@ -4,6 +4,39 @@ using namespace pkg_3;
 
 PrimeNumbers::PrimeNumbers() : Node("prime_numbers")
 {
+  using namespace std::placeholders;
+
+    this->action_server_ = rclcpp_action::create_server<PrimeNumberInterface>(
+      this,
+      "/prime_number",
+      std::bind(&PrimeNumbers::handle_goal, this, _1, _2),
+      std::bind(&PrimeNumbers::handle_cancel, this, _1),
+      std::bind(&PrimeNumbers::handle_accepted, this, _1));
+}
+
+rclcpp_action::GoalResponse PrimeNumbers::handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const PrimeNumberInterface::Goal> goal)
+{
+    RCLCPP_INFO(this->get_logger(), "Received goal to compute %d nth prime number", goal->nth_prime_number);
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse PrimeNumbers::handle_cancel(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle)
+{
+    RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void PrimeNumbers::handle_accepted(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle)
+{
+    using namespace std::placeholders;
+    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+    std::thread{std::bind(&PrimeNumbers::execute, this, _1), goal_handle}.detach();
+}
+
+void PrimeNumbers::execute(const std::shared_ptr<GoalHandlePrimeNumber> goal_handle)
+{
 
 }
 
